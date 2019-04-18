aee91dc23cb0f750896fff1c6ed8c421d96fb6d2
Denise Case <dcase@nwmissouri.edu>
2019-04-18 11:56:22 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# [44-386 Digital Forensics Syllabus](https://profcase.github.io/44-386-digital-forensics-syllabus)
+
+[![Dependency Status](https://img.shields.io/david/nodejs/nodejs.org.svg)](https://david-dm.org/nodejs/nodejs.org)
+[![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-lightgrey.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)
+[![MIT Licensed](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+
+* Denise Case
+* Assistant Professor
+* Northwest Missouri State University
+
+## Links
+
+* Site: <https://profcase.github.io/44-386-digital-forensics-syllabus>
+* Source: <https://github.com/profcase/44-386-digital-forensics-syllabus>
+
+## Recommended
+
+* [Git](https://git-scm.com/download/win) version control system
+* [TortoiseGit](https://tortoisegit.org/) - integrates with Windows File Explorer
+* [Chrome](https://www.google.com/chrome/) web browser
+* [Visual Studio Code](https://code.visualstudio.com/) - editor
+
+## Getting Started
+
+* Fork contents to a new repository.
+* Clone your repository down to your local machine.
+* From Windows File Explorer, open index.html in Chrome to view.
+* Using Visual Studio Code, open the folder to edit.
+* Open GitHub repository, under Settings / GitHub Pages, set Source to master branch and click Save.
+* Go to your Google Analytics dashboard. Under Home / Settings /Admin / Add a new property for your GitHub Page, generate your tracking id, and replace the analytics code with yours.  
---
+++ sw.js
@@ -0,0 +1,79 @@
+console.log("Service worker starting");
+
+var CACHE_NAME = '44-663-v1';
+var urlsToCache = [
+  '/',
+  '/styles/case-syllabus.css',
+  '/scripts/main.js'
+];
+
+self.addEventListener('install', event => {
+  console.log("Service worker install starting");
+  // Perform install steps - open cache, cache files, confirm
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache ', CACHE_NAME);
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => {
+        console.log('Error opening cache ', CACHE_NAME);
+        return;
+      })
+  );
+  console.log("Service worker installed");
+});
+
+self.addEventListener('activate', event => {
+  console.log("Service worker activated");
+});
+
+self.addEventListener('beforeinstallprompt', event => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  event.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = event;
+});
+
+self.addEventListener('fetch', event => {
+  console.log("Service worker got a fetch request");
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request)
+          .then(response => {
+            // Check for valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream 
+            // and its body can only be consumed once.
+            // We need one for the browser and one for the cache.
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              })
+              .catch(err => {
+                console.log('Error opening cache ', CACHE_NAME);
+                return;
+              })
+            return response;
+          })
+          .catch(err => {
+            console.log('Error fetching event request ', event.request);
+            return;
+          })
+      })
+  );
+  console.log("Service worker finished fetch");
+});
+
+
