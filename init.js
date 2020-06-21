fb881b1b8841c66b41a072be7cbae9416fb4a257
Denise Case <denisecase@gmail.com>
2020-06-20 21:01:18 -0500

    data-driven outline

---
+++ init.js
@@ -0,0 +1,12 @@
+// export init object with the initialization data
+
+export default {
+  mod1:
+    'Course intro, digital forensics profession and investigations, facilities, data aquisition (Ch1-3)',
+  mod2: 'Processing scenes, Windows, tools (Ch 4-5)',
+  mod3: 'Tools, Mac systems, Linux systems (Ch 6-7)',
+  mod4: 'Recovery, forensics analysis & validation (Ch 8-9)',
+  mod5: 'VM, cloud, network forensics (Ch 10-11)',
+  mod6: 'Email and mobile forensics (Ch 12-13)',
+  mod7: 'Ethics, review, final submission',
+};
