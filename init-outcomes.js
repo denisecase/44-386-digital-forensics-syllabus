0f4f605b6fc555fc9b7f896c50f9698aca495935
Denise Case <denisecase@gmail.com>
2020-06-22 00:17:56 -0500

    store state; shared elements

---
+++ init-outcomes.js
@@ -0,0 +1,17 @@
+// export init object with the initialization data
+
+export default {
+  outcomesArray: [
+    'Describe the digital forensics profession and investigations.',
+    'Understand how to acquire forensically-sound digital evidence.',
+    'Describe operating systems forensics on Windows and CLI systems.',
+    'Describe operating systems forensics on Macintosh and Linux systems.',
+    'Employ current computer forensics tools.',
+    'Recover "deleted" and/or intentionally hidden information from various types of media.',
+    'Perform computer forensics analysis and validation.',
+    'Describe virtual machine and cloud forensics.',
+    'Describe network forensics and employ live acquisitions.',
+    'Understand and perform email investigations.',
+    'Understand and perform mobile and other device investigations.',
+  ],
+};
